5b819f7a64d21f58f6b4f350a57f81872ccdc420
ameur <ameur.drira@gmail.com>
2017-01-28 14:03:29 +0100

    first commit

---
+++ Consumer.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ameurdrira on 28/01/2017.
+ */
+
+var NRP = require('node-redis-pubsub');
+var time = require('time');
+
+var config = {
+    port: 6379,
+    host: 'soitech.cloudapp.net',
+    auth: 'iot2016'
+};
+var nrp = new NRP(config);
+
+nrp.on('Downlink', function (data) {
+
+    b = new time.Date();
+    console.log(b.toString() + "Downlink msg : " + JSON.stringify(data));
+});
